2dcd4db48e08262e4d3c0ffee7f96bb901c9e3dd
qiuxuntao <qiuxuntao@qiuxuntaodeMacBook-Pro.local>
2022-01-11 21:10:54 +0800

    底部标签栏的路由四大组件的切换

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,30 @@ const routes = [
   {
     path: '/',
     name: 'layout',
-    component: () => import('@/views/layout/index.vue')
+    component: () => import('@/views/layout/index.vue'),
+    children: [
+      {
+        path: '', // 默认子路由 相当于重定向到首页
+        name: 'home',
+        component: () => import('@/views/home/home.vue')
+      },
+      {
+        path: '/qa',
+        name: 'qa',
+        component: () => import('@/views/qa/qa.vue')
+      },
+      {
+        path: '/video',
+        name: 'video',
+        component: () => import('@/views/video/video.vue')
+      },
+      {
+        path: '/my',
+        name: 'my',
+        component: () => import('@/views/my/my.vue')
+      }
+
+    ]
   }
 ]
 
