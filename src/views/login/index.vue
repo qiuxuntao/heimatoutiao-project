48c29461fb8563f7df9f22301a3911ff038c99ea
qiuxuntao <qiuxuntao@qiuxuntaodeMacBook-Pro.local>
2022-01-11 10:45:01 +0800

    手机号验证码

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,18 +5,47 @@
     <!-- /导航栏 -->
 
     <!-- 登录表单 -->
-    <van-form @submit="onSubmit">
+
+    <!--
+      表单验证：
+        1、给 van-field 组件配置 rules 验证规则
+          参考文档：https://youzan.github.io/vant/#/zh-CN/form#rule-shu-ju-jie-gou
+        2、当表单提交的时候会自动触发表单验证
+           如果验证通过，会触发 submit 事件
+           如果验证失败，不会触发 submit
+     -->
+    <van-form ref="loginForm" @submit="onSubmit">
       <!-- 手机号 -->
-      <van-field v-model="user.mobile" name="手机号" placeholder="请输入手机号">
+      <van-field
+        v-model="user.mobile"
+        name="mobile"
+        placeholder="请输入手机号"
+        :rules="userFormRulers.mobile"
+        type="number"
+        maxlength="11"
+      >
         <i slot="left-icon" class="heimatoutiao heimatoutiao-shouji"></i>
       </van-field>
       <!-- 验证码 -->
-      <van-field v-model="user.code" name="验证码" placeholder="请输入验证码">
+      <van-field
+        v-model="user.code"
+        name="code"
+        placeholder="请输入验证码"
+        :rules="userFormRulers.code"
+        type="number"
+        maxlength="6"
+      >
         <i slot="left-icon" class="heimatoutiao heimatoutiao-yanzhengma"></i>
 
         <!-- 发送短信验证码按钮 -->
         <template #button>
-          <van-button  class="send-sms-btn" round size="small" type="default"
+          <van-button
+            class="send-sms-btn"
+            round
+            size="small"
+            type="default"
+            @click="onSendSms"
+            native-type="button"
             >发送验证码</van-button
           >
         </template>
@@ -43,7 +72,18 @@ export default {
     return {
       user: {
         mobile: '', // 手机号
-        code: ''// 验证码
+        code: '' // 验证码
+      },
+      // 配置ruler手机号 手机验证码验证规则
+      userFormRulers: {
+        mobile: [
+          { required: true, message: '手机号不能为空' },
+          { pattern: /^1[3|5|7|8]\d{9}$/, message: '手机号格式错误' }
+        ],
+        code: [
+          { required: true, message: '验证码不能为空' },
+          { pattern: /^\d{6}$/, message: '验证码格式错误' }
+        ]
       }
     }
   },
@@ -56,15 +96,42 @@ export default {
       // 1.获取表单数据(登陆的手机号和验证码)
       const user = this.user
       console.log(user)
+
       // 2.表单验证
+      // 在组件中必须通过this. $toast 来调用Toast 组件
+      this.$toast.loading({
+        message: '加载中...',
+        forbidClick: true, // 禁止背景点击
+        duration: 0 // 持续时间，默认2000，如果为0，持续展示
+      })
       // 3.提交表单请求登陆
       try {
+        // try里面的一定是成功的
         const res = await login(user)
         console.log('登陆成功', res)
+        this.$toast.success('登陆成功')
       } catch (err) {
-        console.log('登陆失败', err)
+        // catch一定是失败的
+        if (err.response.status === 400) {
+          this.$toast.fail('登录失败，手机号或验证码错误')
+        } else {
+          this.$toast.fail('登录失败,请稍后重试')
+        }
       }
       // 4.根据请求响应结果处理后续操作
+    },
+    async onSendSms () {
+      console.log('onSendSms')
+      // 1. 校验手机号
+      console.log(this)
+      try {
+        await this.$refs.loginForm.validate('mobile')
+      } catch (err) {
+        return console.log('验证失败', err)
+      }
+
+      // 2. 验证通过，显示倒计时
+      // 3. 请求发送验证码
     }
   }
 }
@@ -86,7 +153,7 @@ export default {
   .login-btn-wrap {
     padding: 53px 33px;
     .login-btn {
-      background-color: #6db4fb;
+      background-color: orangered;
       border: none;
     }
   }
