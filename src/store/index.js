a176442b915777c5463b8431fcc285a2c0b26e0a
qiuxuntao <qiuxuntao@qiuxuntaodeMacBook-Pro.local>
2022-01-11 17:35:47 +0800

    登陆注册封装本地存储模块

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { getItem, setItem } from '@/utils/storage.js'
 
 Vue.use(Vuex)
 
@@ -8,13 +9,15 @@ const TOKEY_KEY = 'TOUTIAO_USER'
 export default new Vuex.Store({
   state: {
     // 一个对象、在储当前登录用户信息( token等数据)
-    user: JSON.parse(window.localStorage.getItem(TOKEY_KEY))
+    // user: JSON.parse(window.localStorage.getItem(TOKEY_KEY))
+    user: getItem(TOKEY_KEY)
   },
   mutations: {
     setUser (state, data) {
       state.user = data
       // 为了防止刷新丢失，需要把数据备份到本地存储
-      window.localStorage.setItem(TOKEY_KEY, JSON.stringify(state.user))
+      // window.localStorage.setItem(TOKEY_KEY, JSON.stringify(state.user))
+      setItem(TOKEY_KEY, state.user)
     }
   },
   actions: {
---
+++ src/utils/storage.js
@@ -0,0 +1,26 @@
+// 封装本地存储操作模块
+
+// 获取数据
+export const getItem = name => {
+  const data = window.localStorage.getItem(name)
+  try {
+    return JSON.parse(data)
+  } catch (err) {
+    return data
+  }
+}
+
+// 存储数据
+
+export const setItem = (name, value) => {
+  // 将数组、对象类型的数据转换为JSON 格式字符串进行存储
+  if (typeof value === 'object') {
+    value = JSON.stringify(value)
+  }
+  window.localStorage.setItem(name, value)
+}
+
+// 删除数据
+export const removeItem = name => {
+  window.localStorage.removeItem(name)
+}
