a3b2f5fec7c1aff56a1068047d933d9e7de63187
qiuxuntao <qiuxuntao@qiuxuntaodeMacBook-Pro.local>
2022-01-13 18:18:15 +0800

    头部标签动画及滑动

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -13,6 +13,19 @@
       >
     </van-nav-bar>
     <!-- /导航栏 -->
+
+    <!-- 标签栏 -->
+    <van-tabs v-model="active" animated swipeable>
+      <van-tab title="标签 1">内容 1</van-tab>
+      <van-tab title="标签 2">内容 2</van-tab>
+      <van-tab title="标签 3">内容 3</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+      <van-tab title="标签 5">内容 5</van-tab>
+      <van-tab title="标签 6">内容 6</van-tab>
+      <van-tab title="标签 7">内容 7</van-tab>
+      <van-tab title="标签 8">内容 8</van-tab>
+    </van-tabs>
+    <!-- 标签栏 -->
   </div>
 </template>
 
@@ -22,7 +35,8 @@ export default {
   components: {},
   props: {},
   data () {
-    return {}
+    // active是在下标的显示内容
+    return { active: 0 }
   },
   computed: {},
   watch: {},
@@ -40,7 +54,7 @@ export default {
   .search-btn {
     width: 555px;
     height: 64px;
-    background-color: #5babfb;
+    background-color: #73adf9;
     border: none;
     font-size: 28px;
     .van-icon {
